e314be053a72734abc8852d71a9e08d1a33f6674
GabrielASF2 <gabriel_asf2@hotmail.com>
2022-06-22 17:11:17 -0300

    First Commit

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Request, Response, Router } from "express";
+
+import multer from "multer";
+
+const multerConfig = multer();
+
+const router = Router();
+
+router.post(
+    "/products", 
+    multerConfig.single("file"), 
+    (request: Request, response: Response) => {
+        console.log(request.file);
+        return response.send();
+})
+
+export { router };
