63122a3a862ae27693e0d85111d3df6c7111ea2e
GabrielASF2 <gabriel_asf2@hotmail.com>
2022-07-06 16:59:03 -0300

    Incluido o código que insere o csv na tabela do db

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,67 @@
 import { Request, Response, Router } from "express";
-
+import { Readable } from "stream";
+import readline from "readline";
 import multer from "multer";
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+export { client };
+
+
 
 const multerConfig = multer();
 
 const router = Router();
 
+interface Product {
+    code_bar: string; 
+    description: string;
+    price: string;
+    quantity: string;
+}
+
 router.post(
     "/products", 
     multerConfig.single("file"), 
-    (request: Request, response: Response) => {
-        console.log(request.file?.buffer.toString("utf-8"));
-        return response.send();
-})
+    async (request: Request, response: Response) => {
+        const { file } = request;
+        const buffer = file?.buffer;
+
+        const readableFile = new Readable();
+        readableFile.push(buffer);
+        readableFile.push(null);
+
+        const productsLine = readline.createInterface({
+            input: readableFile
+        });
+
+        const products: Product [] = [];
+
+        for await(let line of productsLine ) {
+            const productLineSplit = line.split(",");
+            
+            products.push({
+                code_bar: productLineSplit[0], 
+                description: productLineSplit[1],
+                price: productLineSplit[2],
+                quantity: productLineSplit[3],
+            });
+        }
+
+        for await (let {code_bar, description, price, quantity} of products) {
+            await client.products.create({
+                data: {
+                    code_bar,
+                    description,
+                    price,
+                    quantity,
+                }
+
+            })
+        }
+        
+        return response.json(products);
+
+    }
+);
 
 export { router };
