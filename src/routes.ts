37d80982e1b8b094d2e3cb618c7af6170c863922
GabrielASF2 <gabriel_asf2@hotmail.com>
2022-07-27 15:03:44 -0300

    CSV export add

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,11 @@ import { Request, Response, Router } from "express";
 import { Readable } from "stream";
 import readline from "readline";
 import multer from "multer";
+import { format } from "path";
 require('dotenv').config();
 const multerConfig = multer();
-const xl = require('excel4node');
+const fileSystem = require("fs");
+const csv = require('fast-csv');
 
 const router = Router();
 
@@ -23,43 +25,35 @@ router.post(
             input: readableFile
         });
 
+    
+
         const rtspA = [];
         const rtspB = [];
         const typeA = process.env.TYPE_A;
         const typeB = process.env.TYPE_B;
 
-        
+        const ws2 = fileSystem.createWriteStream("data.csv");
+        const csvStream = csv.format({ headers: ["GID", "URL", "SID", "STATE", "CAMS" ] });
 
-        const wb = new xl.Workbook();
-        const ws = wb.addWorksheet('Worksheet Name');
-
-        const headingColumnNames =["GID", "URL", "SID", "STATE", "CAMS" ];
-        
-        let headingColumnIndex = 1;
-        headingColumnNames.forEach(heading => {ws.cell(1, headingColumnIndex++).string(heading)});
-        
-        let rowIndex =2;
-        let columnIndex = 1;
         for await(let line of productsLine ) {
             const row = line.split(",");
+
+           
            
             rtspA.push(`${row[0]}-${row[8]} ${row[6]}:${row[7]}@${row[4]}:${row[5]}/${typeA} ${row[0]} 2 ${row[9]} `);
             rtspB.push(`${row[0]}-${row[8]} ${row[6]}:${row[7]}@${row[4]}:${row[5]}/${typeB} ${row[0]} 2 ${row[9]} `);
             
-            ws.cell(rowIndex, 1).string(`${row[0]}-${row[8]}`);
-            ws.cell(rowIndex, 2).string(`${row[6]}:${row[7]}@${row[4]}:${row[5]}/${typeA}`);
-            ws.cell(rowIndex, 3).string(`${row[0]}`);
-            ws.cell(rowIndex, 4).string(`2`);
-            ws.cell(rowIndex, 5).string(`${row[9]}`);
-            rowIndex = rowIndex +1;
+            csvStream.write({
+                GID: `${row[0]}-${row[8]}`, 
+                URL: `rtsp://${row[6]}:${row[7]}@${row[4]}:${row[5]}/${typeB}`, 
+                SID: `${row[0]}`, 
+                STATE: '2', 
+                CAMS: `${row[9]}` });
         }
+        csvStream.pipe(ws2).on('end', () => process.exit());
+        csvStream.end();
 
-
-        wb.write('rtsp.xlsx');
-
-
-
-        return response.json(rtspA);
+      return response.json(rtspB);
 
     }
 );
